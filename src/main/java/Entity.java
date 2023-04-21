3ee78806e48aff4d867338a9db3407f3d408ad7d
Jan Soth <jan.soth@gmx.net>
2023-04-21 14:14:41 +0200

    add Test for Entity Class

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -5,20 +5,43 @@ public abstract class Entity {
     //private final String name;
     private int hitpoints = 100;
     private boolean isAlive = true;
-    private int damageUnarmed;
-    private boolean canUseItems;
     private Weapon weapon;
+    private Planet homePlanet;
 
     // constructor
+    public Entity(Planet homePlanet, String name, Weapon weapon) {
 
+    }
 
     // getter methods
 
     // setter methods
 
     // Instance methods
+    public abstract int takeDamage();
 
 
+    public Planet getHomePlanet(){
+        return this.homePlanet;
+    };
 
+    public String getName(){
+        return "";
+    }
 
+    public int getHitpoints(){
+        return 0;
+    };
+
+    public boolean isAlive(){
+        return false;
+    };
+
+    public Weapon getWeapon(){
+        return Weapon.NONE;
+    };
+
+    public int getDamageUnarmed(){
+        return -1;
+    };
 }
--- src/test/java/EntityTest.java
+++ src/test/java/EntityTest.java
@@ -1,11 +1,41 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EntityTest {
 
-    @Test
-    public void testEntity(){
+    // Create concrete class that extends Entity in order to test i.e. constructor
+    public class ConcreteEntity extends Entity{
+        public ConcreteEntity(Planet homePlanet, String name, Weapon weapon) {
+            super(homePlanet, name, weapon);
+        }
 
+        @Override
+        public int takeDamage(){
+            return -1;
+        }
+    };
+
+    private ConcreteEntity entity;
+
+    @BeforeEach
+    public void beforeEachTest(){
+        Planet homePlanet = Planet.MARS;
+        String name = "Entity1";
+        Weapon weapon = Weapon.SHOTGUN;
+
+        entity = new ConcreteEntity(homePlanet, name, weapon);
+    }
+
+    @Test
+    public void testEntityConstructor(){
+        assertAll(
+                () -> assertEquals(Planet.MARS, entity.getHomePlanet()),
+                () -> assertEquals("Entity1", entity.getName()),
+                () -> assertEquals(100, entity.getHitpoints()),
+                () -> assertEquals(true, entity.isAlive()),
+                () -> assertEquals(Weapon.SHOTGUN, entity.getWeapon())
+        );
     }
 }
