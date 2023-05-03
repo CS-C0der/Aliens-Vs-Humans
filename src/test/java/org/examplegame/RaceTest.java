799e84ad6c3228fb82ecc7b010963cb16c2ec954
Jan Soth <jan.soth@gmx.net>
2023-05-03 17:08:29 +0200

    simplify randomRace in RaceTest

--- src/test/java/org/examplegame/RaceTest.java
+++ src/test/java/org/examplegame/RaceTest.java
@@ -1,5 +1,6 @@
 package org.examplegame;
 
+import org.examplegame.entities.Alien;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -23,51 +24,25 @@ class RaceTest {
 
     @Test
     public void testRandomRace(){
+        // ToDo refactor: Alien hat methode getRaceCount!!
 
-        System.out.println("List of Races:" + Race.getRacesExZombie());
 
-        Race randomRace;
-
-        // create Map to memorize amount of Race created per Race
-        Map<Race, Integer> countPerRace = new HashMap<>();
-        // initialize all values with 0
-        for (Race weapon : Race.getRacesExZombie()){
-            countPerRace.put(weapon, 0);
+        // generate many random aliens
+        for (int i = 0; i < 50; i++){
+            new Alien(Race.randomRace());  // no need to store object. just create object to increase static variable raceCount in Alien class
         }
 
-        int currentAmount;
+        // iterate races
+        for (Race race : Race.values()) {
+            if (race == Race.ZOMBIE){
+                if (Alien.getRaceCount(race) > 0){ fail("no Zombies should be returned by randomRace!"); }
+            // not Zombie
+            } else if (Alien.getRaceCount(race) == 0){
+                fail("no Alien of Race " + race + "created");
+            }
+            System.out.println("Number of " + race +"s :" + Alien.getRaceCount(race));
+        }
 
-        // generate many random weapons
-        for (int i = 0; i < 50; i++){
-            randomRace = Race.randomRace();
-
-            // memorize amount of how often a specific weapon is generated
-            switch (randomRace){
-
-                case FACEHUGGER -> {
-                    currentAmount = countPerRace.get(Race.FACEHUGGER);
-                    countPerRace.replace(Race.FACEHUGGER, currentAmount +1);
-                }
-                case KLINGON -> {
-                    currentAmount = countPerRace.get(Race.KLINGON);
-                    countPerRace.replace(Race.KLINGON, currentAmount +1);
-                }
-                case BORG -> {
-                    currentAmount = countPerRace.get(Race.BORG);
-                    countPerRace.replace(Race.BORG, currentAmount +1);
-                }
-                case ALF -> {
-                    currentAmount = countPerRace.get(Race.ALF);
-                    countPerRace.replace(Race.ALF, currentAmount +1);
-                }
-                default -> {
-                    fail("Zombie should not be possible");
-                }
-            } // End of switch Statement
-
-        } // End of for loop
-
-        System.out.println(countPerRace);
     }
 
 
